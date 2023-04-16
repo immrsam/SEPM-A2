44fb13265ca4cb66d3949f6b1aa07c5891a2eabf
s3826714 <67183686+s3826714@users.noreply.github.com>
2023-04-16 16:45:39 +1000

    Delete a2/src/a2 directory

--- a2/src/a2/StaffMember.java
+++
@@ -1,7 +0,0 @@
-package a2;
-
-public class StaffMember extends User {
-	public StaffMember(int userId, String fName, String lName, String email, String phoneNumber ,String password) {
-		super(userId, fName, lName, email, phoneNumber, password);
-	}
-}
