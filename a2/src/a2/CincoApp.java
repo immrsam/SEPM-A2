744fae52ab8ca466a86277b8f03a8a87b7db54d8
s3826714 <67183686+s3826714@users.noreply.github.com>
2023-04-16 16:46:19 +1000

    Debugging and changes to loadData

---
+++ a2/src/a2/CincoApp.java
@@ -0,0 +1,42 @@
+package a2;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CincoApp {
+	protected static HashMap<String, StaffMember> staff = new HashMap<String, StaffMember>();
+	protected static HashMap<String, Technician> tech = new HashMap<String, Technician>();
+	
+	protected static HashMap<Integer, Ticket> tickets = new HashMap<Integer, Ticket>();
+	protected static HashMap<Integer, Technician> ticketTracker = new HashMap<Integer, Technician>();
+	
+	public static void main(String[] args) {
+		// console scanner
+		Scanner input = new Scanner (System.in);
+		
+		// variables
+		int userID = 0;
+		boolean appRunning = true;
+		
+		// Load data
+		LoadData.LoadStaff();
+		LoadData.LoadTechnicians();
+		
+		do {
+			userID = LoginMenu.Processor(input);
+			
+			if (Integer.toString(userID).charAt(0) == '1') {
+				// userID is for technician
+				TechMenu.Processor(input, userID);				
+			} else if (Integer.toString(userID).charAt(0) == '2') {
+				// userID is for staff
+				StaffMenu.Processor(input, userID);
+			}
+		} while (appRunning);
+		
+		input.close();
+	}
+	
+	
+
+}
---
+++ a2/src/a2/StaffMember.java
@@ -0,0 +1,7 @@
+package a2;
+
+public class StaffMember extends User {
+	public StaffMember(int userID, String fName, String lName, String email, String phoneNumber ,String password) {
+		super(userID, fName, lName, email, phoneNumber, password);
+	}
+}
